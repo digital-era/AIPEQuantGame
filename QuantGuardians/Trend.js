135bf51385e7bd8c31e582fa11cb0b297bfdf296
digital-era <32516133+digital-era@users.noreply.github.com>
2026-02-02 14:20:34 +0800

    Update Trend.js

--- QuantGuardians/Trend.js
+++ QuantGuardians/Trend.js
@@ -198,7 +198,7 @@ function openDetailChart(item, color) {
         
         // 为移动端添加选项
         const optionsList = [
-            { value: '1min',      label: '1分价格' },
+            { value: '1min',      label: '分钟价格' },
             { value: '30d_price', label: '30天价格' },
             { value: '30d_pot',   label: 'PotScore' },
             { value: '30d_super', label: '超大单%' },
